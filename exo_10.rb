c324b9b87f19090bb14aab24aec4943bcaebe16b
Robinson <dubois.robinson@hotmail.fr>
2019-01-04 11:51:23 +0100

    exo 10 et 11

---
+++ exo_10.rb
@@ -0,0 +1,3 @@
+puts "Quel est ton année de naissance ?"
+age = gets.chomp.to_i
+puts "#{2017 - age.to_i } est ton age"
---
+++ exo_11.rb
@@ -0,0 +1,5 @@
+puts "choisis un nombre"
+user_nomber = gets.chomp.to_i
+user_nomber.to_i.times do
+puts "Salut ça farte ?"
+end
